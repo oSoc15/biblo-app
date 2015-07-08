ec1d7cdabcd77587a17da18bebee147cec20bb51
Ruben Meul <ruben.meul@kahosl.be>
2015-07-08 21:25:33 +0200

    Basic get recommended books updated.

--- Server/index.php
+++ Server/index.php
@@ -20,20 +20,61 @@ $app['debug'] = true;
 
 //Get recommended books.
 $app->get('/',function(){
+  //read the xml from the file
   $xml=simplexml_load_file("dump.xml") or die("Error: Cannot create object.");
+
+  //convert the xml to json
   $json = json_encode($xml);
+
+  //convert the json to an array
   $results = json_decode($json,TRUE);
 
-  for ($x = 0; $x <= 8; $x++) {
-    $temp = $results["result"][rand(0,sizeof($results)-1)];
-  }
+  //will hold the final books
+  $output = [];
 
+  //pick 8 (for now random) books
+  for ($x = 0; $x <= 8; $x++) {
+    //random index
+    $result = $results['result'][rand(0, sizeof($results['result'])-1)];
+
+    //set array
+    $temp = [
+      "coverimage" => $result['coverimage']['url'],
+      "title" => $result['titles']['short-title']
+    ];
+
+    //check if author is set
+    if(array_key_exists('author', $result)){
+      $temp["author"] = $result['author']['main-author'];
+    }
+    else{
+      $temp["author"] = "Geen auteur te vinden.";
+    }
+
+    //check if summary is set
+    if(array_key_exists('summaries', $result)){
+      $temp["description"] = $result['summaries']['summary'];
+    }
+    else{
+      $temp["description"] = "Geen beschrijving te vinden.";
+    }
+    array_push($output, $temp);
+
+    //check if genres is set
+    if(is_string($result['genres'])){
+      $temp["genres"] = array_unique($result['genres']['genre']);
+    }else{
+      $temp["genres"] = $result['genres']['genre'];
+    }
+  }
+  //encode the array to json and return it
+  return json_encode($output);
 });
 
 //Get test books.
 $app->get('/test',function(){
   $arr = [
-      "book1" => [
+      [
         "coverimage" => "http://webservices.bibliotheek.be/index.php?func=cover&ISBN=9789031709069&VLACCnr=1053077",
         "title" => "Vergif",
         "author" => "R.H. Schoemans",
@@ -42,7 +83,7 @@ $app->get('/test',function(){
         ],
         "description" => "Bij toeval vindt Erwin het lijk van een man. Het gaat om een afrekening van de IRA met een 'verrader', die werkt in een Britse kazerne in de Belgische Kempen. Op zoek naar de dader van de moord komt Erwin in Belfast terecht, bij Daffy, een Ierse jongen, die een jaar eerder bij hem op vakantie is geweest. Soms raken ze verzeild in allerlei verwikkelingen. In dit spannende verhaal zijn de politieke situatie in Noord-Ierland en de gevolgen hiervan voor de bevolking sterk verweven. Het woordgebruik en de zinsbouw is afgestemd op de leeftijdsgroep van 12 tot 14 jaar. In het chronologisch verteld verhaal gaan de, op verschillende plaatsen gesitueerde gebeurtenissen, logisch in elkaar over. De auteur probeert met dit boek bij de jeugd meer inzicht te krijgen in de achtergronden en situatie in Noord-Ierland. De informatie is qua beleving en interesse afgestemd op de beoogde lezersgroep (12-14 jaar)."
       ],
-      "book2" => [
+      [
         "coverimage" => "http://webservices.bibliotheek.be/index.php?func=cover&ISBN=9789031709069&VLACCnr=1053077",
         "title" => "Vergif",
         "author" => "R.H. Schoemans",
@@ -51,7 +92,7 @@ $app->get('/test',function(){
         ],
         "description" => "Bij toeval vindt Erwin het lijk van een man. Het gaat om een afrekening van de IRA met een 'verrader', die werkt in een Britse kazerne in de Belgische Kempen. Op zoek naar de dader van de moord komt Erwin in Belfast terecht, bij Daffy, een Ierse jongen, die een jaar eerder bij hem op vakantie is geweest. Soms raken ze verzeild in allerlei verwikkelingen. In dit spannende verhaal zijn de politieke situatie in Noord-Ierland en de gevolgen hiervan voor de bevolking sterk verweven. Het woordgebruik en de zinsbouw is afgestemd op de leeftijdsgroep van 12 tot 14 jaar. In het chronologisch verteld verhaal gaan de, op verschillende plaatsen gesitueerde gebeurtenissen, logisch in elkaar over. De auteur probeert met dit boek bij de jeugd meer inzicht te krijgen in de achtergronden en situatie in Noord-Ierland. De informatie is qua beleving en interesse afgestemd op de beoogde lezersgroep (12-14 jaar)."
       ],
-      "book3" => [
+      [
         "coverimage" => "http://webservices.bibliotheek.be/index.php?func=cover&ISBN=9789031709069&VLACCnr=1053077",
         "title" => "Vergif",
         "author" => "R.H. Schoemans",
@@ -60,7 +101,7 @@ $app->get('/test',function(){
         ],
         "description" => "Bij toeval vindt Erwin het lijk van een man. Het gaat om een afrekening van de IRA met een 'verrader', die werkt in een Britse kazerne in de Belgische Kempen. Op zoek naar de dader van de moord komt Erwin in Belfast terecht, bij Daffy, een Ierse jongen, die een jaar eerder bij hem op vakantie is geweest. Soms raken ze verzeild in allerlei verwikkelingen. In dit spannende verhaal zijn de politieke situatie in Noord-Ierland en de gevolgen hiervan voor de bevolking sterk verweven. Het woordgebruik en de zinsbouw is afgestemd op de leeftijdsgroep van 12 tot 14 jaar. In het chronologisch verteld verhaal gaan de, op verschillende plaatsen gesitueerde gebeurtenissen, logisch in elkaar over. De auteur probeert met dit boek bij de jeugd meer inzicht te krijgen in de achtergronden en situatie in Noord-Ierland. De informatie is qua beleving en interesse afgestemd op de beoogde lezersgroep (12-14 jaar)."
       ],
-      "book4" => [
+      [
         "coverimage" => "http://webservices.bibliotheek.be/index.php?func=cover&ISBN=9789031709069&VLACCnr=1053077",
         "title" => "Vergif",
         "author" => "R.H. Schoemans",
@@ -69,7 +110,7 @@ $app->get('/test',function(){
         ],
         "description" => "Bij toeval vindt Erwin het lijk van een man. Het gaat om een afrekening van de IRA met een 'verrader', die werkt in een Britse kazerne in de Belgische Kempen. Op zoek naar de dader van de moord komt Erwin in Belfast terecht, bij Daffy, een Ierse jongen, die een jaar eerder bij hem op vakantie is geweest. Soms raken ze verzeild in allerlei verwikkelingen. In dit spannende verhaal zijn de politieke situatie in Noord-Ierland en de gevolgen hiervan voor de bevolking sterk verweven. Het woordgebruik en de zinsbouw is afgestemd op de leeftijdsgroep van 12 tot 14 jaar. In het chronologisch verteld verhaal gaan de, op verschillende plaatsen gesitueerde gebeurtenissen, logisch in elkaar over. De auteur probeert met dit boek bij de jeugd meer inzicht te krijgen in de achtergronden en situatie in Noord-Ierland. De informatie is qua beleving en interesse afgestemd op de beoogde lezersgroep (12-14 jaar)."
       ],
-      "book5" => [
+      [
         "coverimage" => "http://webservices.bibliotheek.be/index.php?func=cover&ISBN=9789031709069&VLACCnr=1053077",
         "title" => "Vergif",
         "author" => "R.H. Schoemans",
@@ -78,7 +119,7 @@ $app->get('/test',function(){
         ],
         "description" => "Bij toeval vindt Erwin het lijk van een man. Het gaat om een afrekening van de IRA met een 'verrader', die werkt in een Britse kazerne in de Belgische Kempen. Op zoek naar de dader van de moord komt Erwin in Belfast terecht, bij Daffy, een Ierse jongen, die een jaar eerder bij hem op vakantie is geweest. Soms raken ze verzeild in allerlei verwikkelingen. In dit spannende verhaal zijn de politieke situatie in Noord-Ierland en de gevolgen hiervan voor de bevolking sterk verweven. Het woordgebruik en de zinsbouw is afgestemd op de leeftijdsgroep van 12 tot 14 jaar. In het chronologisch verteld verhaal gaan de, op verschillende plaatsen gesitueerde gebeurtenissen, logisch in elkaar over. De auteur probeert met dit boek bij de jeugd meer inzicht te krijgen in de achtergronden en situatie in Noord-Ierland. De informatie is qua beleving en interesse afgestemd op de beoogde lezersgroep (12-14 jaar)."
       ],
-      "book6" => [
+      [
         "coverimage" => "http://webservices.bibliotheek.be/index.php?func=cover&ISBN=9789031709069&VLACCnr=1053077",
         "title" => "Vergif",
         "author" => "R.H. Schoemans",
@@ -87,7 +128,7 @@ $app->get('/test',function(){
         ],
         "description" => "Bij toeval vindt Erwin het lijk van een man. Het gaat om een afrekening van de IRA met een 'verrader', die werkt in een Britse kazerne in de Belgische Kempen. Op zoek naar de dader van de moord komt Erwin in Belfast terecht, bij Daffy, een Ierse jongen, die een jaar eerder bij hem op vakantie is geweest. Soms raken ze verzeild in allerlei verwikkelingen. In dit spannende verhaal zijn de politieke situatie in Noord-Ierland en de gevolgen hiervan voor de bevolking sterk verweven. Het woordgebruik en de zinsbouw is afgestemd op de leeftijdsgroep van 12 tot 14 jaar. In het chronologisch verteld verhaal gaan de, op verschillende plaatsen gesitueerde gebeurtenissen, logisch in elkaar over. De auteur probeert met dit boek bij de jeugd meer inzicht te krijgen in de achtergronden en situatie in Noord-Ierland. De informatie is qua beleving en interesse afgestemd op de beoogde lezersgroep (12-14 jaar)."
       ],
-      "book7" => [
+      [
         "coverimage" => "http://webservices.bibliotheek.be/index.php?func=cover&ISBN=9789031709069&VLACCnr=1053077",
         "title" => "Vergif",
         "author" => "R.H. Schoemans",
@@ -96,7 +137,7 @@ $app->get('/test',function(){
         ],
         "description" => "Bij toeval vindt Erwin het lijk van een man. Het gaat om een afrekening van de IRA met een 'verrader', die werkt in een Britse kazerne in de Belgische Kempen. Op zoek naar de dader van de moord komt Erwin in Belfast terecht, bij Daffy, een Ierse jongen, die een jaar eerder bij hem op vakantie is geweest. Soms raken ze verzeild in allerlei verwikkelingen. In dit spannende verhaal zijn de politieke situatie in Noord-Ierland en de gevolgen hiervan voor de bevolking sterk verweven. Het woordgebruik en de zinsbouw is afgestemd op de leeftijdsgroep van 12 tot 14 jaar. In het chronologisch verteld verhaal gaan de, op verschillende plaatsen gesitueerde gebeurtenissen, logisch in elkaar over. De auteur probeert met dit boek bij de jeugd meer inzicht te krijgen in de achtergronden en situatie in Noord-Ierland. De informatie is qua beleving en interesse afgestemd op de beoogde lezersgroep (12-14 jaar)."
       ],
-      "book8" => [
+      [
         "coverimage" => "http://webservices.bibliotheek.be/index.php?func=cover&ISBN=9789031709069&VLACCnr=1053077",
         "title" => "Vergif",
         "author" => "R.H. Schoemans",
