3b97bfcb488d42d0c8beab9d344f9455bb3f3dbc
Wesley Vanbrabant <wesley@wesobi.be>
2015-07-09 09:43:57 +0200

    changes to the mockup, loading data in (index2.html, etc) through the json file

---
+++ mockup/scripts/script.js
@@ -0,0 +1,38 @@
+/**
+ * Created by vanbrabantwesley on 8/07/15.
+ */
+
+$(document).ready(function() {
+
+    //after button is clicked we download the data
+        //start ajax request
+        $.ajax({
+            type: 'get',
+            url: "../server/index.php",
+            //force to handle it as text
+            dataType: "json",
+            success: function(data) {
+
+                $.each(data, function(index, book){
+                    var index = index;
+                    var title = book.title;
+                    var image = book.coverimage;
+                    var author = book.author;
+                    var description = book.description;
+
+
+                    $('#page-wrapper').append(
+                        "<div class='col-md-3 col-lg-3 book' data-id=" + index + ">" +
+                        "<img src=" + image + " alt="+ title +" class="+ "resize" + " >" +
+                        "</div>"
+                    );
+
+
+                });
+
+
+            }
+
+        });
+
+    });
---
+++ mockup/scripts/script2.js
@@ -0,0 +1,3 @@
+/**
+ * Created by vanbrabantwesley on 8/07/15.
+ */
